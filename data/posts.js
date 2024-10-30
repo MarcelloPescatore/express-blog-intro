91f5515209686a07651d2ba95cb921c742137737
MarcelloPescatore <pescatoremarcello@gmail.com>
2024-10-30 16:10:57 +0100

    assignment 3

---
+++ data/posts.js
@@ -0,0 +1,37 @@
+// creo l'array di posts
+const posts = [
+    {
+        title: 'titolo1',
+        content: 'contenuto1',
+        img: 'https://www.greenplanner.it/wp-content/uploads/2021/09/zucca.jpg',
+        tags: ['tag1', 'tag2', 'tag3']
+    },
+    {
+        title: 'titolo2',
+        content: 'contenuto2',
+        img: 'https://www.greenplanner.it/wp-content/uploads/2021/09/zucca.jpg',
+        tags: ['tag1', 'tag2', 'tag3']
+    },
+    {
+        title: 'titolo3',
+        content: 'contenuto3',
+        img: 'https://www.greenplanner.it/wp-content/uploads/2021/09/zucca.jpg',
+        tags: ['tag1', 'tag2', 'tag3']
+    },
+    {
+        title: 'titolo4',
+        content: 'contenuto4',
+        img: 'https://www.greenplanner.it/wp-content/uploads/2021/09/zucca.jpg',
+        tags: ['tag1', 'tag2', 'tag3']
+    },
+    {
+        title: 'titolo5',
+        content: 'contenuto5',
+        img: 'https://www.greenplanner.it/wp-content/uploads/2021/09/zucca.jpg',
+        tags: ['tag1', 'tag2', 'tag3']
+    }
+]
+
+// esporto l'array posts
+module.exports = posts
+
